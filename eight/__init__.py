c282c4425427de96add2a329e2e75f310ba7ea6c
Andrey Kislyuk <kislyuk@gmail.com>
2014-03-12 00:01:32 -0700

    Continue

--- eight/__init__.py
+++ eight/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import sys
+import sys, itertools
 
 # Reminder: Don't put any globals here. They will become unusable once we perform the loader trick below.
 
@@ -11,10 +11,16 @@ class Loader(object):
         self.__path__ = __path__
         self.USING_PYTHON2 = True if sys.version_info < (3, 0) else False
         if self.USING_PYTHON2:
+            from itertools import ifilter as filter, imap as map, izip as zip
             self._map = dict(str=unicode,
                              bytes=str,
                              input=raw_input,
-                             int=long)
+                             int=long,
+                             chr=unichr,
+                             range=xrange,
+                             filter=itertools.ifilter,
+                             map=itertools.imap,
+                             zip=itertools.izip)
             self._manifest = list(self._map.keys())
             self._moves = {'queue': 'Queue',
                            'reprlib': 'repr'}
@@ -22,7 +28,12 @@ class Loader(object):
             self._map = dict(str=str,
                              bytes=bytes,
                              input=input,
-                             int=int)
+                             int=int,
+                             chr=chr,
+                             range=range,
+                             filter=filter,
+                             map=map,
+                             zip=zip)
             self._manifest = list(self._map.keys())
 
     def __getattr__(self, attr):

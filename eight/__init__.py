12414009c840c5b26a06a7425a70b438a6215b8d
Andrey Kislyuk <kislyuk@gmail.com>
2014-03-23 11:49:16 -0700

    Experimental stdio wrapping functionality

--- eight/__init__.py
+++ eight/__init__.py
@@ -35,6 +35,10 @@ class Loader(object):
                              map=map,
                              zip=zip)
             self._manifest = list(self._map.keys())
+        self._map.update(dict(USING_PYTHON2=self.USING_PYTHON2,
+                              PY2=self.USING_PYTHON2,
+                              PY3=not self.USING_PYTHON2))
+        self._stdio_wrapped = False
 
     def __getattr__(self, attr):
         if attr == '__all__':
@@ -55,6 +59,22 @@ class Loader(object):
                 attr = self._moves[attr]
             __import__(attr)
             return self._sys.modules[attr]
+
+    def wrap_stdio(self):
+        if self.USING_PYTHON2 and not self._stdio_wrapped:
+            import sys, io
+            from io import TextIOWrapper
+            class StderrTextIOWrapper(TextIOWrapper):
+                def write(self, s):
+                    if type(s) is unicode:
+                        TextIOWrapper.write(self, s)
+                    else:
+                        TextIOWrapper.write(self, unicode(s, self.encoding))
+
+            sys.stdin = io.open(sys.stdin.fileno(), encoding=sys.stdin.encoding)
+            sys.stdout = StderrTextIOWrapper(io.FileIO(sys.stdout.fileno(), mode='w'), encoding=sys.stdout.encoding)
+            sys.stderr = StderrTextIOWrapper(io.FileIO(sys.stderr.fileno(), mode='w'), encoding=sys.stderr.encoding)
+            self._stdio_wrapped = True
 
 loader = Loader()
 sys.modules[__name__] = loader

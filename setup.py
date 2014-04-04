5e74eb408909d4d92be14612ce66793bb0e18a15
Andrey Kislyuk <kislyuk@gmail.com>
2014-04-03 20:48:55 -0700

    Add 3.4

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Development Status :: 4 - Beta',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
